d240877117ec15f0600d1b3b1039d62ca7bb52e0
Théo Guérin <theo.guerin.pro@gmail.com>
2016-02-05 00:37:25 +0100

    ajustement du client de test

--- clientTest.py
+++ clientTest.py
@@ -1,21 +1,27 @@
 #! /bin/ipython3
 
+import time
 import socket
 
-HOST, PORT = "localhost", 9999
+ip = "localhost"
+port = 7777
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def client(ip, port):
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    message = "mess no:"
+    cpt= 1
+    while True:
+        try:
+            sock.sendall(bytes(message+str(cpt), 'ascii'))
+            response = str(sock.recv(1024), 'ascii')
+            print("Received: {}".format(response))
+            cpt+=1
+            time.sleep(1)
+        finally:
+            pass
+            #sock.close()
 
-for i in range(10):
 
-    data=input()
-
-    sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-    received = str(sock.recv(1024), "utf-8")
-
-    print("Sent:     {}".format(data))
-    print("Received: {}".format(received))
+client(ip, port)
